58fc2cf1dafeab920d33a1c0952b66bfc0f271cd
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2023-08-03 18:51:28 +0000

    Separated the key-extend code
    Moved the code for extending a key into a separate function to allow for re-use in future ciphers, as well as to improve readability.
    
    Modified:     src/lightman210567_encipher/encipher.py

--- src/lightman210567_encipher/encipher.py
+++ src/lightman210567_encipher/encipher.py
@@ -1,26 +1,23 @@
+def extendKey(targetLength, key):
+  difference = targetLength - len(key)
+  keyList = list(key)
+  keyBoundary = (len(key) - 1)
+
+  keyPosition = 0
+  while difference > 0:
+    keyToAdd = key[keyPosition]
+    keyList.append(keyToAdd)
+    difference -= 1
+
+    if keyPosition == keyBoundary:
+      keyPosition = 0
+    else:
+      keyPosition += 1
+  return "".join(keyList)
+
 def vigenereCipher(plainText, key):
   plainTextLength = len(plainText)
   keyLength = len(key)
 
-  # Check if the key is long enough for the plain text
-  # If the key is not long enough, repeat the key until the text and key are equal lengths
   if plainTextLength > keyLength:
-    difference = plainTextLength - keyLength
-
-    keyList = list(key)
-    keyBoundary = keyLength - 1
-
-    keyPosition = 0
-    # while the difference in length is not 0, append a part of the key to the end of the list
-    # This repeats the key until the text and key have the same length
-    while difference != 0:
-      keyToAdd = keyList[keyPosition]
-      keyList.append(keyToAdd)
-      difference -= 1
-
-      # Check if the last key part has been used.
-      # If the key has been used, reset the position to start from the beginning.
-      if keyPosition == keyBoundary:
-        keyPosition = 0
-      else:
-        keyPosition += 1
+    extendKey(plainTextLength, key)
