16fa9d5400a8f23966af2cf195d3243d3cf896ba
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2023-08-03 17:06:10 +0000

    Created checks to ensure long enough keys
    Added code that checks if the entered plain text and the key are the same length. If they are not the same length, a key value is taken from the original and appended to the end. This ensured that keys are *always* long enough to complete vigenere encryption.
    
    Modified:     src/lightman210567_encipher/encipher.py

--- src/lightman210567_encipher/encipher.py
+++ src/lightman210567_encipher/encipher.py
@@ -1,2 +1,26 @@
 def vigenereCipher(plainText, key):
-  print("Placeholder")
+  plainTextLength = len(plainText)
+  keyLength = len(key)
+
+  # Check if the key is long enough for the plain text
+  # If the key is not long enough, repeat the key until the text and key are equal lengths
+  if plainTextLength > keyLength:
+    difference = plainTextLength - keyLength
+
+    keyList = list(key)
+    keyBoundary = keyLength - 1
+
+    keyPosition = 0
+    # while the difference in length is not 0, append a part of the key to the end of the list
+    # This repeats the key until the text and key have the same length
+    while difference != 0:
+      keyToAdd = keyList[keyPosition]
+      keyList.append(keyToAdd)
+      difference -= 1
+
+      # Check if the last key part has been used.
+      # If the key has been used, reset the position to start from the beginning.
+      if keyPosition == keyBoundary:
+        keyPosition = 0
+      else:
+        keyPosition += 1
