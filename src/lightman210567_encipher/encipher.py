fa1cd874aeb6dbde783cd93f40a20919270a9ebc
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2023-08-04 18:30:33 +0000

    Added space character support
    Added support for a space character into the vigenere cipher  as well as the number-letter converters and key extender.
    
    Bug discovered: key extend does not add a space when the original key length is greater than the space's index.
    
    Modified:     src/lightman210567_encipher/encipher.py

--- src/lightman210567_encipher/encipher.py
+++ src/lightman210567_encipher/encipher.py
@@ -1,18 +1,23 @@
-def extendKey(targetLength, key):
+def extendKey(plainText, key):
+  targetLength = len(plainText)
   difference = targetLength - len(key)
   keyList = list(key)
   keyBoundary = (len(key) - 1)
 
+  i = len(key)
   keyPosition = 0
   while difference > 0:
-    keyToAdd = key[keyPosition]
+    if plainText[i] == " ":
+      keyToAdd = " "
+    else:
+      keyToAdd = key[keyPosition]
+      if keyPosition == keyBoundary:
+        keyPosition = 0
+      else:
+        keyPosition += 1
     keyList.append(keyToAdd)
     difference -= 1
-
-    if keyPosition == keyBoundary:
-      keyPosition = 0
-    else:
-      keyPosition += 1
+    i += 1
 
   return "".join(keyList)
 
@@ -20,7 +25,10 @@ def stringToNumbers(string):
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   numberList = []
   for letter in string:
-    number = alphabet.find(letter)
+    if letter == " ":
+      number = 1000
+    else:
+      number = alphabet.find(letter)
     numberList.append(number)
   return numberList
 
@@ -28,28 +36,35 @@ def numbersToString(numbers):
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   letterList = []
   for number in numbers:
-    letter = alphabet[int(number)]
+    if number == 1000:
+      letter = " "
+    else:
+      letter = alphabet[int(number)]
     letterList.append(letter)
   return "".join(letterList)
 
 def vigenereCipher(plainText, key):
   plainTextLength = len(plainText)
   keyLength = len(key)
-
   if plainTextLength > keyLength:
-    key = extendKey(plainTextLength, key)
+    key = extendKey(plainText, key)
 
   numberList = stringToNumbers(plainText)
   numericKey = stringToNumbers(key)
-
   cipherNumbers = []
   
   for i in range(0, plainTextLength):
     plainNumber = numberList[i]
     shift = numericKey[i]
-    
-    shiftedNumber = (plainNumber + shift) % 26
-    cipherNumbers.append(shiftedNumber)
+
+    if plainNumber == 1000:
+      cipherNumbers.append(1000)
+    else:
+      shiftedNumber = (plainNumber + shift) % 26
+      cipherNumbers.append(shiftedNumber)
 
   cipherText = numbersToString(cipherNumbers)
   return cipherText
+
+test = vigenereCipher("this is a test of the vigenere cipher", "secure")
+print(test)
