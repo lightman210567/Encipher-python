c2613c928db1d0ffa7836b733e683a46dacf20df
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2023-08-06 12:04:04 +0000

    Transfered the string-number conversion functions to their own file

--- src/encipherpy/encipher.py
+++ src/encipherpy/encipher.py
@@ -1,24 +1,6 @@
-def stringToNumbers(string):
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
-  numberList = []
-  for letter in string.lower():
-    if letter == " ":
-      number = 1000
-    else:
-      number = alphabet.find(letter)
-    numberList.append(number)
-  return numberList
 
-def numbersToString(numbers):
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
-  letterList = []
-  for number in numbers:
-    if number == 1000:
-      letter = " "
-    else:
-      letter = alphabet[int(number)]
-    letterList.append(letter)
-  return "".join(letterList)
+
+
 
 def vigenereCipher(plainText, key, encrypt = True):
   plainTextLength = len(plainText)
