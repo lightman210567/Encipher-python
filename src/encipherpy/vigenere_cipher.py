9456b41ed2cffc78bd579cb4b10ca835031fe1aa
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2023-08-06 12:07:16 +0000

    Transfered the vigenere cipher to its own file

---
+++ src/encipherpy/vigenere_cipher.py
@@ -0,0 +1,23 @@
+def vigenereCipher(plainText, key, encrypt = True):
+  plainTextLength = len(plainText)
+  key = rebuildKey(plainText, key)
+
+  numberList = stringToNumbers(plainText)
+  numericKey = stringToNumbers(key)
+  cipherNumbers = []
+  
+  for i in range(0, plainTextLength):
+    plainNumber = numberList[i]
+    shift = numericKey[i]
+
+    if plainNumber == 1000:
+      cipherNumbers.append(1000)
+    else:
+      if encrypt == True:
+        shiftedNumber = (plainNumber + shift) % 26
+      else:
+        shiftedNumber = (plainNumber - shift) % 26
+      cipherNumbers.append(shiftedNumber)
+
+  cipherText = numbersToString(cipherNumbers)
+  return cipherText
