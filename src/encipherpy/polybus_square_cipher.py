40dc97de2e98240a1ac80ad22ccab4d519f8412c
lightman210567 <81330314+lightman210567@users.noreply.github.com>
2023-09-04 12:17:35 +0100

    fixed the polybus square cipher by removing punctuation
    fixed polybus square decryption by completely removing all foreign characters (non alphabetic).
    these characters were offsetting the number pairs and making the string impossible to decrypt, this has been fixed.
    
    punctuation and spaces are no longer supported in the polybus square cipher
    
    modified: src/encipherpy/polybus_square_cipher.py
    
    Signed-off-by: lightman210567 <81330314+lightman210567@users.noreply.github.com>

--- src/encipherpy/polybus_square_cipher.py
+++ src/encipherpy/polybus_square_cipher.py
@@ -1,5 +1,6 @@
 def polybusSquare(plainText, encrypt = True):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
+    numbers = "12345"
     square = [
         ["a", "b", "c", "d", "e"],
         ["f", "g", "h", "i", "k"],
@@ -8,23 +9,31 @@ def polybusSquare(plainText, encrypt = True):
         ["v", "w", "x", "y", "z"]
     ]
     cipherText = []
+    plainText = list(plainText)
+
+    # detect all foreign characters
+    foreignCharacters = []
+    for character in plainText:
+        if encrypt == True: # if encrypting, remove all non alphabetic characters
+            if character not in alphabet:
+                foreignCharacters.append(character)
+        else:
+            if character not in numbers: # if decrypting, remove all non numeric characters
+                foreignCharacters.append(character)
+    # remove all foreign characters
+    for character in foreignCharacters:
+        plainText.remove(character)
 
     if encrypt == True:
         for letter in plainText:
-            if letter not in alphabet:
-                cipherText.append(letter)
-            else:
-                index = findInSquare(square, letter)
-                cipherText.append(index)
+            index = findInSquare(square, letter)
+            cipherText.append(index)
     else:
         for i in range(0, len(plainText), 2):
-            if plainText[i] not in alphabet:
-                cipherText.append(plainText[i])
-            else:
-                row = int(plainText[i]) - 1
-                column = int(plainText[i + 1]) - 1
-                letter = square[row][column]
-                cipherText.append(letter)
+            row = int(plainText[i]) - 1
+            column = int(plainText[i + 1]) - 1
+            letter = square[row][column]
+            cipherText.append(letter)
 
     cipherText = "".join(cipherText)
     return cipherText
